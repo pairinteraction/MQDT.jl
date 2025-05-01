743bab39992839aedee13356dc5ee3c80b45dc2b
Sebastian Weber <seweber@posteo.de>
2025-05-01 11:04:27 +0200

    Delete the manifest, separate dependencies of the examples from the library, show usefull commands in the readme, several fixes to generate_pairinteraction_database

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # MQDT
 
-[![Build Status](https://github.com/pairinteraction/MQDT.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/pairinteraction/MQDT.jl/actions/workflows/CI.yml?query=branch%3Amain)
+[![Build Status](https://github.com/atom-pairinteraction/MQDT.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/atom-pairinteraction/MQDT.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+## Running tests from within the repository
+
+```bash
+julia --project=. -e 'using Pkg; Pkg.test()'
+```
+
+## Running examples from within the repository
+
+```bash
+julia --project=examples -e 'import Pkg; Pkg.instantiate(); include("examples/generate_lu_fano.jl")'
+```
--- examples/generate_pairinteraction_database.jl
+++ examples/generate_pairinteraction_database.jl
@@ -3,20 +3,19 @@ using MQDT
 # choose species
 SP = :Yb174
 
-# generate dispatch tables
+# generate dispatch tables # TODO check whether the MODELS_TABLE is not missing any models
 const MODELS_TABLE = Dict(
-    :Sr87 => [MQDT.Sr87.MODEL_S0, MQDT.Sr87.MODEL_S1, 
-              MQDT.Sr87.MODEL_P0, MQDT.Sr87.MODEL_P1, MQDT.Sr87.MODEL_P2, 
-              MQDT.Sr87.MODEL_D1, MQDT.Sr87.MODEL_D2, MQDT.Sr87.MODEL_D3],
+    :Sr87 => [MQDT.Sr87.MODEL_S35, MQDT.Sr87.MODEL_S45, 
+              MQDT.Sr87.MODEL_P25, MQDT.Sr87.MODEL_P35, MQDT.Sr87.MODEL_P45, MQDT.Sr87.MODEL_P55, MQDT.Sr87.MODEL_P65,
+              MQDT.Sr87.MODEL_D15, MQDT.Sr87.MODEL_D25, MQDT.Sr87.MODEL_D35, MQDT.Sr87.MODEL_D45, MQDT.Sr87.MODEL_D55, MQDT.Sr87.MODEL_D65, MQDT.Sr87.MODEL_D75],
     :Sr88 => [MQDT.Sr88.MODEL_S0, MQDT.Sr88.MODEL_S1,
               MQDT.Sr88.MODEL_P0, MQDT.Sr88.MODEL_P1, MQDT.Sr88.MODEL_P2, 
               MQDT.Sr88.MODEL_D1, MQDT.Sr88.MODEL_D2, MQDT.Sr88.MODEL_D3],
-    :Yb171 => [MQDT.Yb171.MODEL_S0, MQDT.Yb171.MODEL_S1,
-               MQDT.Yb171.MODEL_P0, MQDT.Yb171.MODEL_P1, MQDT.Yb171.MODEL_P2,
-               MQDT.Yb171.MODEL_D1, MQDT.Yb171.MODEL_D2, MQDT.Yb171.MODEL_D3],
-    :Yb173 => [MQDT.Yb173.MODEL_S0, MQDT.Yb173.MODEL_S1,
-               MQDT.Yb173.MODEL_P0, MQDT.Yb173.MODEL_P1, MQDT.Yb173.MODEL_P2,
-               MQDT.Yb173.MODEL_D1, MQDT.Yb173.MODEL_D2, MQDT.Yb173.MODEL_D3],
+    :Yb171 => [MQDT.Yb171.MODEL_S05, MQDT.Yb171.MODEL_S15,
+               MQDT.Yb171.MODEL_P05, MQDT.Yb171.MODEL_P15, MQDT.Yb171.MODEL_P25,
+               MQDT.Yb171.MODEL_D05, MQDT.Yb171.MODEL_D15, MQDT.Yb171.MODEL_D25, MQDT.Yb171.MODEL_D35],
+    :Yb173 => [MQDT.Yb173.MODEL_S15, MQDT.Yb173.MODEL_S25, MQDT.Yb173.MODEL_S35,
+               MQDT.Yb173.MODEL_P05, MQDT.Yb173.MODEL_P15, MQDT.Yb173.MODEL_P25, MQDT.Yb173.MODEL_P35, MQDT.Yb173.MODEL_P45],
     :Yb174 => [MQDT.Yb174.MODEL_S0, MQDT.Yb174.MODEL_S1,
                MQDT.Yb174.MODEL_P0, MQDT.Yb174.MODEL_P1, MQDT.Yb174.MODEL_P2,
                MQDT.Yb174.MODEL_D1, MQDT.Yb174.MODEL_D2, MQDT.Yb174.MODEL_D3],
@@ -33,11 +32,11 @@ const PARA_TABLE = Dict(
 n1, n2 = 35, 40 # above n ~ 105 will cause problems with the current integrator for matrix elements
 models = MODELS_TABLE[SP]
 para = PARA_TABLE[SP]
-c_states = MQDT.eigenstates(1.5, 2.5, MODEL_G1, para) # clock states
+c_states = MQDT.eigenstates(1.5, 2.5, MQDT.Yb174.MODEL_G1, para) # clock states # TODO  make it work for SP != Yb174
 r_states = [MQDT.eigenstates(n1, n2, M, para) for M in models] # Rydberg states
 
 # generate state table
-c_basis = MQDT.basisarray(c_states, MODEL_G1)
+c_basis = MQDT.basisarray(c_states, MQDT.Yb174.MODEL_G1) # TODO make it work for SP != Yb174
 r_basis = MQDT.basisarray(r_states, models)
 basis = union(c_basis, r_basis)
 state_table = MQDT.state_data(basis, para)
@@ -55,7 +54,7 @@ mm = MQDT.matrix_data(dm)
 md = MQDT.matrix_data(dd)
 
 # prepare PAIRINTERACTION output
-c_db = MQDT.databasearray(c_states, MODEL_G1)
+c_db = MQDT.databasearray(c_states, MQDT.Yb174.MODEL_G1) # TODO make it work for SP != Yb174
 r_db = MQDT.databasearray(r_states, models)
 b_db = union(c_db, r_db)
 ST = MQDT.state_data(b_db, para)
@@ -68,16 +67,16 @@ MD = MQDT.tri_to_full(md, ST)
 
 # store tables as csv files
 using CSV
-CSV.write("data/$(SP)_mqdt_states.csv", ST)
-CSV.write("data/$(SP)_mqdt_matrix_elements_d.csv", M1)
-CSV.write("data/$(SP)_mqdt_matrix_elements_q.csv", M2)
-CSV.write("data/$(SP)_mqdt_matrix_elements_mu.csv", MM)
-CSV.write("data/$(SP)_mqdt_matrix_elements_q0.csv", MD)
+CSV.write("$(SP)_mqdt_states.csv", ST)
+CSV.write("$(SP)_mqdt_matrix_elements_d.csv", M1)
+CSV.write("$(SP)_mqdt_matrix_elements_q.csv", M2)
+CSV.write("$(SP)_mqdt_matrix_elements_mu.csv", MM)
+CSV.write("$(SP)_mqdt_matrix_elements_q0.csv", MD)
 
 # store tables as parquet files
 using Parquet2
-Parquet2.writefile("data/$(SP)_mqdt_states.parquet", ST)
-Parquet2.writefile("data/$(SP)_mqdt_matrix_elements_d.parquet", M1)
-Parquet2.writefile("data/$(SP)_mqdt_matrix_elements_q.parquet", M2)
-Parquet2.writefile("data/$(SP)_mqdt_matrix_elements_mu.parquet", MM)
-Parquet2.writefile("data/$(SP)_mqdt_matrix_elements_q0.parquet", MD)
+Parquet2.writefile("$(SP)_mqdt_states.parquet", ST)
+Parquet2.writefile("$(SP)_mqdt_matrix_elements_d.parquet", M1)
+Parquet2.writefile("$(SP)_mqdt_matrix_elements_q.parquet", M2)
+Parquet2.writefile("$(SP)_mqdt_matrix_elements_mu.parquet", MM)
+Parquet2.writefile("$(SP)_mqdt_matrix_elements_q0.parquet", MD)
