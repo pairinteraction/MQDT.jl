30899da4f529d0a8c799cb81f72d2f8f8ef86ab1
Frederic Hummel <frederic@atom-computing.com>
2025-06-24 15:20:22 -0700

    update test according to new naming convention for models

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
 
 @testset "MQDT.jl" begin
     # TODO replace the following code with proper tests
-    s = MQDT.eigenstates(24, 30, MQDT.Yb171.RYDBERG_S15, MQDT.Yb171.PARA)
-    b = MQDT.basisarray([s], [MQDT.Yb171.RYDBERG_S15])
+    s = MQDT.eigenstates(24, 30, MQDT.Yb171.FMODEL_HIGHN_S15, MQDT.Yb171.PARA)
+    b = MQDT.basisarray([s], [MQDT.Yb171.FMODEL_HIGHN_S15])
     m = diag(MQDT.matrix_element(MQDT.Yb171.PARA, b))
 end
