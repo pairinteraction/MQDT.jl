db9cf900f76b6ff33014491d9a6163d36db0237c
Frederic Hummel <frederic@atom-computing.com>
2025-06-24 15:16:20 -0700

    update examples, remove LaTeXStrings package

--- examples/generate_pairinteraction_database.jl
+++ examples/generate_pairinteraction_database.jl
@@ -5,20 +5,20 @@ SP = :Yb174
 
 # generate dispatch tables # TODO check whether the MODELS_TABLE is not missing any models
 const MODELS_TABLE = Dict(
-    :Sr87 => [MQDT.Sr87.MODEL_S35, MQDT.Sr87.MODEL_S45, 
-              MQDT.Sr87.MODEL_P25, MQDT.Sr87.MODEL_P35, MQDT.Sr87.MODEL_P45, MQDT.Sr87.MODEL_P55, MQDT.Sr87.MODEL_P65,
-              MQDT.Sr87.MODEL_D15, MQDT.Sr87.MODEL_D25, MQDT.Sr87.MODEL_D35, MQDT.Sr87.MODEL_D45, MQDT.Sr87.MODEL_D55, MQDT.Sr87.MODEL_D65, MQDT.Sr87.MODEL_D75],
-    :Sr88 => [MQDT.Sr88.FMODEL_S0, MQDT.Sr88.FMODEL_S1,
-              MQDT.Sr88.FMODEL_P0, MQDT.Sr88.FMODEL_P1, MQDT.Sr88.FMODEL_P2, 
-              MQDT.Sr88.FMODEL_D1, MQDT.Sr88.FMODEL_D2, MQDT.Sr88.FMODEL_D3],
-    :Yb171 => [MQDT.Yb171.RYDBERG_S05, MQDT.Yb171.RYDBERG_S15,
-               MQDT.Yb171.RYDBERG_P05, MQDT.Yb171.RYDBERG_P15, MQDT.Yb171.RYDBERG_P25,
-               MQDT.Yb171.RYDBERG_D05, MQDT.Yb171.RYDBERG_D15, MQDT.Yb171.RYDBERG_D25, MQDT.Yb171.RYDBERG_D35],
-    :Yb173 => [MQDT.Yb173.MODEL_S15, MQDT.Yb173.MODEL_S25, MQDT.Yb173.MODEL_S35,
-               MQDT.Yb173.MODEL_P05, MQDT.Yb173.MODEL_P15, MQDT.Yb173.MODEL_P25, MQDT.Yb173.MODEL_P35, MQDT.Yb173.MODEL_P45],
-    :Yb174 => [MQDT.Yb174.RYDBERG_S0, MQDT.Yb174.RYDBERG_S1,
-               MQDT.Yb174.RYDBERG_P0, MQDT.Yb174.RYDBERG_P1, MQDT.Yb174.RYDBERG_P2,
-               MQDT.Yb174.RYDBERG_D1, MQDT.Yb174.RYDBERG_D2, MQDT.Yb174.RYDBERG_D3],
+    :Sr87 => [MQDT.Sr87.FMODEL_HIGHN_S35, MQDT.Sr87.FMODEL_HIGHN_S45, 
+              MQDT.Sr87.FMODEL_HIGHN_P25, MQDT.Sr87.FMODEL_HIGHN_P35, MQDT.Sr87.FMODEL_HIGHN_P45, MQDT.Sr87.FMODEL_HIGHN_P55, MQDT.Sr87.FMODEL_HIGHN_P65,
+              MQDT.Sr87.FMODEL_HIGHN_D15, MQDT.Sr87.FMODEL_HIGHN_D25, MQDT.Sr87.FMODEL_HIGHN_D35, MQDT.Sr87.FMODEL_HIGHN_D45, MQDT.Sr87.FMODEL_HIGHN_D55, MQDT.Sr87.FMODEL_HIGHN_D65, MQDT.Sr87.FMODEL_HIGHN_D75],
+    :Sr88 => [MQDT.Sr88.FMODEL_HIGHN_S0, MQDT.Sr88.FMODEL_HIGHN_S1,
+              MQDT.Sr88.FMODEL_HIGHN_P0, MQDT.Sr88.FMODEL_HIGHN_P1, MQDT.Sr88.FMODEL_HIGHN_P2, 
+              MQDT.Sr88.FMODEL_HIGHN_D1, MQDT.Sr88.FMODEL_HIGHN_D2, MQDT.Sr88.FMODEL_HIGHN_D3],
+    :Yb171 => [MQDT.Yb171.FMODEL_HIGHN_S05, MQDT.Yb171.FMODEL_HIGHN_S15,
+               MQDT.Yb171.FMODEL_HIGHN_P05, MQDT.Yb171.FMODEL_HIGHN_P15, MQDT.Yb171.FMODEL_HIGHN_P25,
+               MQDT.Yb171.FMODEL_HIGHN_D05, MQDT.Yb171.FMODEL_HIGHN_D15, MQDT.Yb171.FMODEL_HIGHN_D25, MQDT.Yb171.FMODEL_HIGHN_D35],
+    :Yb173 => [MQDT.Yb173.FMODEL_HIGHN_S15, MQDT.Yb173.FMODEL_HIGHN_S25, MQDT.Yb173.FMODEL_HIGHN_S35,
+               MQDT.Yb173.FMODEL_HIGHN_P05, MQDT.Yb173.FMODEL_HIGHN_P15, MQDT.Yb173.FMODEL_HIGHN_P25, MQDT.Yb173.FMODEL_HIGHN_P35, MQDT.Yb173.FMODEL_HIGHN_P45],
+    :Yb174 => [MQDT.Yb174.FMODEL_HIGHN_S0, MQDT.Yb174.FMODEL_HIGHN_S1,
+               MQDT.Yb174.FMODEL_HIGHN_P0, MQDT.Yb174.FMODEL_HIGHN_P1, MQDT.Yb174.FMODEL_HIGHN_P2,
+               MQDT.Yb174.FMODEL_HIGHN_D1, MQDT.Yb174.FMODEL_HIGHN_D2, MQDT.Yb174.FMODEL_HIGHN_D3],
 )
 const PARA_TABLE = Dict(
     :Sr87 => MQDT.Sr87.PARA,
@@ -29,14 +29,14 @@ const PARA_TABLE = Dict(
 )
 
 # calculate bound states
-n1, n2 = 35, 40 # above n ~ 105 will cause problems with the current integrator for matrix elements
+n1, n2 = 35, 40 # above n ~ 95 will cause problems with the current call of hypergeometric functions from the GSL package
 models = MODELS_TABLE[SP]
 para = PARA_TABLE[SP]
-c_states = MQDT.eigenstates(1.5, 2.5, MQDT.Yb174.CORE_P0, para) # clock states # TODO  make it work for SP != Yb174
+c_states = MQDT.eigenstates(1.5, 2.5, MQDT.Yb174.FMODEL_LOWN_P0, para) # clock states # TODO  make it work for SP != Yb174
 r_states = [MQDT.eigenstates(n1, n2, M, para) for M in models] # Rydberg states
 
 # generate state table
-c_basis = MQDT.basisarray(c_states, MQDT.Yb174.CORE_P0) # TODO make it work for SP != Yb174
+c_basis = MQDT.basisarray(c_states, MQDT.Yb174.FMODEL_LOWN_P0) # TODO make it work for SP != Yb174
 r_basis = MQDT.basisarray(r_states, models)
 basis = union(c_basis, r_basis)
 state_table = MQDT.state_data(basis, para)
@@ -54,7 +54,7 @@ mm = MQDT.matrix_data(dm)
 md = MQDT.matrix_data(dd)
 
 # prepare PAIRINTERACTION output
-c_db = MQDT.databasearray(c_states, MQDT.Yb174.CORE_P0) # TODO make it work for SP != Yb174
+c_db = MQDT.databasearray(c_states, MQDT.Yb174.FMODEL_LOWN_P0) # TODO make it work for SP != Yb174
 r_db = MQDT.databasearray(r_states, models)
 b_db = union(c_db, r_db)
 ST = MQDT.state_data(b_db, para)
