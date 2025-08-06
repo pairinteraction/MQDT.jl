1ad6d105910779b2e1ebaa1a1a4c281b957064ed
Frederic Hummel <frederic@atom-computing.com>
2025-08-05 17:37:50 -0700

    update examples

--- examples/generate_lu_fano.jl
+++ examples/generate_lu_fano.jl
@@ -1,3 +1,7 @@
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
 using LinearAlgebra
 using SparseArrays
 using CGcoefficient
@@ -38,15 +42,15 @@ end
 
 # S series of Yb171
 f = 0.5
-s_S05 = MQDT.eigenstates(24, 129, MQDT.Yb171.FMODEL_HIGHN_S05, MQDT.Yb171.PARA)
-b_S05 = MQDT.basisarray([s_S05], [MQDT.Yb171.FMODEL_HIGHN_S05])
-m_S05 = diag(MQDT.matrix_element(MQDT.Yb171.PARA, b_S05))
+s_S05 = eigenstates(24, 129, MQDT.Yb171.FMODEL_HIGHN_S05, MQDT.Yb171.PARA)
+b_S05 = basisarray([s_S05], [MQDT.Yb171.FMODEL_HIGHN_S05])
+m_S05 = diag(matrix_element(MQDT.Yb171.PARA, b_S05))
 g_S05 = -2m_S05 / f * wigner3j(1, 0, f, f, f, f)
 
 f = 1.5
-s_S15 = MQDT.eigenstates(24, 129, MQDT.Yb171.FMODEL_HIGHN_S15, MQDT.Yb171.PARA)
-b_S15 = MQDT.basisarray([s_S15], [MQDT.Yb171.FMODEL_HIGHN_S15])
-m_S15 = diag(MQDT.matrix_element(MQDT.Yb171.PARA, b_S15))
+s_S15 = eigenstates(24, 129, MQDT.Yb171.FMODEL_HIGHN_S15, MQDT.Yb171.PARA)
+b_S15 = basisarray([s_S15], [MQDT.Yb171.FMODEL_HIGHN_S15])
+m_S15 = diag(matrix_element(MQDT.Yb171.PARA, b_S15))
 g_S15 = -2m_S15 / f * wigner3j(1, 0, f, f, f, f)
 
 scatter(layout = (2, 1), size = (400, 500), legend = :topleft)
@@ -74,15 +78,15 @@ savefig("Yb171_S_series.pdf")
 
 # P series of Yb171
 f = 0.5
-s_P05 = MQDT.eigenstates(10, 70, MQDT.Yb171.FMODEL_HIGHN_P05, MQDT.Yb171.PARA)
-b_P05 = MQDT.basisarray([s_P05], [MQDT.Yb171.FMODEL_HIGHN_P05])
-m_P05 = diag(MQDT.matrix_element(MQDT.Yb171.PARA, b_P05))
+s_P05 = eigenstates(10, 70, MQDT.Yb171.FMODEL_HIGHN_P05, MQDT.Yb171.PARA)
+b_P05 = basisarray([s_P05], [MQDT.Yb171.FMODEL_HIGHN_P05])
+m_P05 = diag(matrix_element(MQDT.Yb171.PARA, b_P05))
 g_P05 = -2m_P05 / f * wigner3j(1, 0, f, f, f, f)
 
 f = 1.5
-s_P15 = MQDT.eigenstates(10, 70, MQDT.Yb171.FMODEL_HIGHN_P15, MQDT.Yb171.PARA)
-b_P15 = MQDT.basisarray([s_P15], [MQDT.Yb171.FMODEL_HIGHN_P15])
-m_P15 = diag(MQDT.matrix_element(MQDT.Yb171.PARA, b_P15))
+s_P15 = eigenstates(10, 70, MQDT.Yb171.FMODEL_HIGHN_P15, MQDT.Yb171.PARA)
+b_P15 = basisarray([s_P15], [MQDT.Yb171.FMODEL_HIGHN_P15])
+m_P15 = diag(matrix_element(MQDT.Yb171.PARA, b_P15))
 g_P15 = -2m_P15 / f * wigner3j(1, 0, f, f, f, f)
 
 scatter(layout = (2, 2), size = (700, 500))
