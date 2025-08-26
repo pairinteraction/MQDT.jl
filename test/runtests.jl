1fffaa6428fcb80442bd2bf4e90cfb7bd5435d18
Frederic Hummel <frederic@atom-computing.com>
2025-08-26 16:42:44 -0700

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,5 @@ wigner_init_float(13, "Jmax", 9) # initialize Wigner symbol caluclation
     # TODO replace the following code with proper tests
     s = MQDT.eigenstates(24, 30, MQDT.Yb171.FMODEL_HIGHN_S15, MQDT.Yb171.PARA)
     b = MQDT.basisarray([s], [MQDT.Yb171.FMODEL_HIGHN_S15])
-    m = diag(MQDT.matrix_element(MQDT.Yb171.PARA, b))
+    m = [MQDT.multipole_moments(i, i, MQDT.Yb171.PARA)[4] for i in b.states]
 end
