73c8c34d439f1261d05bba33806e3ea5be73a349
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-11-10 18:21:55 +0100

    mv test model functions inside test/ and run them

---
+++ test/small_test.jl
@@ -0,0 +1,10 @@
+using MQDT
+using CGcoefficient
+
+wigner_init_float(13, "Jmax", 9) # initialize Wigner symbol caluclation
+
+@testset "small test" begin
+    s = MQDT.eigenstates(24, 30, MQDT.Yb171.FMODEL_HIGHN_S15, MQDT.Yb171.PARA)
+    b = MQDT.basisarray([s], [MQDT.Yb171.FMODEL_HIGHN_S15])
+    m = [MQDT.multipole_moments(i, i, MQDT.Yb171.PARA)[4] for i in b.states]
+end
