935b627c38d9bee9229cf65c9b8279bb11044d66
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-12-25 22:37:20 +0100

    [tests] improve test model

--- test/test_model.jl
+++ test/test_model.jl
@@ -1,6 +1,7 @@
 using MQDT
+using PythonCall
 
-function test_model(T::fModel)
+function test_model_struct(T::fModel)
     if (T.size != length(T.terms)) || (T.size != length(T.core)) || (T.size != size(T.defects, 1))
         println("Model size does not correspond to provided parameters.")
         @test false
@@ -19,7 +20,7 @@ function test_model(T::fModel)
     end
 end
 
-function test_model(T::kModel)
+function test_model_struct(T::kModel)
     if (T.size != length(T.terms)) ||
        (T.size != length(T.jjscheme)) ||
        (T.size != length(T.lschannels.i)) ||
@@ -36,19 +37,19 @@ function test_model(T::kModel)
     end
 end
 
-function test_model(T::Union{Vector{fModel},Vector{kModel}})
+function test_model_struct(T::Union{Vector{fModel},Vector{kModel}})
     for i in eachindex(T)
-        test_model(T[i])
+        test_model_struct(T[i])
     end
 end
 
-function test_unitary(T::fModel, P::Parameters)
+function test_model_unitary(T::fModel, P::Parameters)
     c = findall(T.core)
     t0 = T.unitary[c, c]
 
     if any(qn -> isnan(qn.Jc), T.outer_channels.i) ||
        (typeof(T.outer_channels) == fjChannels && any(qn -> isnan(qn.Fc), T.outer_channels.i))
-        println("Skip test_unitary for model $(T.name) due to NaN in quantum numbers.")
+        println("Skip test_model_unitary for model $(T.name) due to NaN in quantum numbers.")
         return
     end
 
@@ -79,13 +80,34 @@ function test_unitary(T::fModel, P::Parameters)
     @test isapprox(t0, t1; rtol=0.001)
 end
 
-function test_unitary(T::kModel)
+function test_model_unitary(T::kModel)
     # TODO not working yet due to NaNs in quantum numbers
-    println("Skip test_unitary for kModel $(T.name).")
+    println("Skip test_model_unitary for kModel $(T.name).")
     return
     t1 = eigen(T.K0).vectors
     t2 = MQDT.matrix_ls_to_jj(T.lschannels, T.jjchannels)'
     @test isapprox(t1, t2; rtol=0.001)
+end
+
+function test_model_states_and_matrix_elements(model::fModel, param::Parameters)
+    nu_min, nu_max = get_nu_limits_from_model(model)
+    nu_max = isnan(nu_max) ? nu_min + 5 : min(nu_max, nu_min + 5)
+    states = eigenstates(nu_min, nu_max, model, param)
+    basis = basisarray(states, model)
+    @test size(basis) == length(states)
+
+    logging = pyimport("logging")
+    logging.getLogger("").setLevel(logging.ERROR)
+    me = matrix_elements(basis, param)
+    @test length(me["dipole"]) == 0
+    @test length(me["quadrupole"]) > 0 || length(me["diamagnetic"]) > 0
+
+    qns0 = model.outer_channels.i[1]
+    if qns0.F != 0
+        @test length(me["paramagnetic"]) >= size(basis)
+    else
+        @test length(me["paramagnetic"]) == 0
+    end
 end
 
 @testset "Models" begin
@@ -102,11 +124,12 @@ end
             println("Testing $(species) $(nm)")
             @testset "$(species) $(nm)" begin
                 if isa(obj, fModel)
-                    test_model(obj)
-                    test_unitary(obj, species.PARA)
+                    test_model_struct(obj)
+                    test_model_unitary(obj, species.PARA)
+                    test_model_states_and_matrix_elements(obj, species.PARA)
                 elseif isa(obj, kModel)
-                    test_model(obj)
-                    test_unitary(obj)
+                    test_model_struct(obj)
+                    test_model_unitary(obj)
                 end
             end
         end
